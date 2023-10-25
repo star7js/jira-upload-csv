93d5bca044c41fdf8747d997ddef1d24fb01a49c
Josh <126814341+star7js@users.noreply.github.com>
2023-10-25 12:47:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Jira Issue Creator
+
+This project provides scripts to automate the creation of Jira issues and their respective sub-tasks using data from CSV
+files.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Project Structure](#project-structure)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Read data from CSV files to extract Jira issue details.
+- Automated creation of Jira issues.
+- Automated creation of sub-tasks linked to the main issues.
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/project_name.git
+
+2. Navigate to the project directory:
+   cd project_name
+
+3. Install the required dependencies:
+pip install -r requirements.txt
+
+## Usage
+
+1. Update the Jira authentication details in the respective script.
+2. Place your CSV files in the `data/` directory.
+3. Run the script:
+python src/script_name.py
+
+## Project Structure
+project_name/
+│
+├── README.md            
+├── requirements.txt     
+│
+├── src/                 
+│   ├── script1.py       
+│   ├── script2.py       
+│   └── ...              
+│
+├── tests/               
+│   ├── test_script1.py  
+│   ├── test_script2.py  
+│   └── ...              
+│
+└── data/                
+    ├── data1.csv
+    └── ...
+
+
+For more details, please refer to the [project structure](#project-structure) section.
+
+## Contributing
+
+Pull requests are welcome! For major changes, please open an issue first to discuss what you'd like to change.
+
+## License
+
+This project is licensed under the MIT License. See `LICENSE` file for details.
---
+++ multi-issue-upload-with-subtasks.py
@@ -0,0 +1,59 @@
+import csv
+from atlassian import Jira
+
+BASE_URL = 'https://your-jira-instance-url'
+jira = Jira(
+    url=BASE_URL,
+    username='your-username',
+    password='your-api-token'
+)
+
+
+def read_csv(file_path):
+    with open(file_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
+
+
+def create_jira_issue(issue_data):
+    return jira.issue_create(fields=issue_data)
+
+
+def create_jira_subtask(subtask_data):
+    return jira.issue_create(fields=subtask_data)
+
+
+def main():
+    rows = read_csv('path_to_your_csv_file.csv')
+    for row in rows:
+        issue_data = {
+            'project': {'key': row['Project Key']},
+            'summary': row['Summary'],
+            'description': row['Description'],
+            'issuetype': {'name': row['Issue Type']}
+        }
+
+        # Create the main issue in Jira
+        main_issue = create_jira_issue(issue_data)
+
+        # Construct the issue URL
+        issue_url = f"{BASE_URL}/browse/{main_issue['key']}"
+        print(f"Issue {row['Summary']} created with Key: {main_issue['key']} and URL: {issue_url}")
+
+        # Create a subtask and link it to the main issue
+        subtask_data = {
+            'project': {'key': row['Project Key']},
+            'summary': row['Subtask Summary'],
+            'description': row['Subtask Description'],
+            'issuetype': {'name': 'Sub-task'},  # Assuming the issuetype for subtasks is named 'Sub-task'
+            'parent': {'id': main_issue['id']}
+        }
+
+        subtask = create_jira_subtask(subtask_data)
+        subtask_url = f"{BASE_URL}/browse/{subtask['key']}"
+        print(
+            f"Subtask {row['Subtask Summary']} created with Key: {subtask['key']} and URL: {subtask_url} linked to main issue {main_issue['key']}")
+
+
+if __name__ == '__main__':
+    main()
